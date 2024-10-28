addda2150c02bd185b0d9ea4a52de77698f6ff59
Zee045 <167368409+Zee045@users.noreply.github.com>
2024-10-28 16:14:09 -0400

    Update sandwich_maker.py

--- Assignment2/sandwich_maker.py
+++ Assignment2/sandwich_maker.py
@@ -5,7 +5,15 @@ class SandwichMaker:
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+        """Checks if resources are sufficient for the order."""
+        for item, amount in ingredients.items():
+            if amount > self.resources.get(item, 0):
+                print(f"Sorry, there is not enough {item}.")
+                return False
+        return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+        """Deducts the required ingredients from resources and confirms sandwich preparation."""
+        for item, amount in ingredients.items():
+            self.resources[item] -= amount
+        print("Your sandwich is ready. Bon appétit!")
